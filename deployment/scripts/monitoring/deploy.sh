9e9fd8d6017898d10b08978b1cf0fda8a64f654e
Gh-Badr <ghazaoui.badr.tech@gmail.com>
2024-12-23 12:25:34 +0100

    Deploy Grafana using the Helm chart

--- deployment/scripts/monitoring/deploy.sh
+++ deployment/scripts/monitoring/deploy.sh
@@ -3,7 +3,8 @@ set -e
 
 # Variables
 NAMESPACE="monitoring"
-VALUES_FILE="deployment/helm/prometheus/values.yml"
+PROMETHEUS_VALUES_FILE="deployment/helm/prometheus/values.yml"
+GRAFANA_VALUES_FILE="deployment/helm/grafana/values.yml"
 
 # Create the monitoring namespace
 kubectl create namespace ${NAMESPACE} || true # Ignore error if the namespace already exists
@@ -12,8 +13,13 @@ kubectl create namespace ${NAMESPACE} || true # Ignore error if the namespace al
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+# Add the Grafana Helm repository
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
 # Install or upgrade Prometheus using the custom values file
-helm upgrade --install prometheus prometheus-community/prometheus --namespace ${NAMESPACE} -f ${VALUES_FILE}
+helm upgrade --install prometheus prometheus-community/prometheus --namespace ${NAMESPACE} -f ${PROMETHEUS_VALUES_FILE}
+helm upgrade --install grafana grafana/grafana --namespace ${NAMESPACE} -f ${GRAFANA_VALUES_FILE}
 
 # Wait for Prometheus pods to be ready
 kubectl wait --for=condition=ready pods --all -n ${NAMESPACE} --timeout=300s
