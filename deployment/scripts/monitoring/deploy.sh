f81020ca5731ab5066acb0dd3126e182ac7f399f
Gh-Badr <ghazaoui.badr.tech@gmail.com>
2024-12-23 14:01:37 +0100

    Switch to kube-prometheus-stack to deploy the monitoring stack

--- deployment/scripts/monitoring/deploy.sh
+++ deployment/scripts/monitoring/deploy.sh
@@ -3,25 +3,19 @@ set -e
 
 # Variables
 NAMESPACE="monitoring"
-PROMETHEUS_VALUES_FILE="deployment/helm/prometheus/values.yml"
-GRAFANA_VALUES_FILE="deployment/helm/grafana/values.yml"
+VALUES_FILE="deployment/helm/kube-prometheus-stack/values.yml"
 
 # Create the monitoring namespace
 kubectl create namespace ${NAMESPACE} || true # Ignore error if the namespace already exists
 
-# Add the Prometheus Helm repository
+# Add the prometheus-community Helm repository
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-# Add the Grafana Helm repository
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-
-# Install or upgrade Prometheus using the custom values file
-helm upgrade --install prometheus prometheus-community/prometheus --namespace ${NAMESPACE} -f ${PROMETHEUS_VALUES_FILE}
-helm upgrade --install grafana grafana/grafana --namespace ${NAMESPACE} -f ${GRAFANA_VALUES_FILE}
+# Deploy monitoring stack
+helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack --namespace ${NAMESPACE} -f ${VALUES_FILE}
 
 # Wait for Prometheus pods to be ready
 kubectl wait --for=condition=ready pods --all -n ${NAMESPACE} --timeout=300s
 
-echo "Prometheus deployed successfully."
+echo "Monitoring stack deployed successfully"
