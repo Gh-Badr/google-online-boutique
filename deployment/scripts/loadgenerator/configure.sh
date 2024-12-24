9e69f899dc3afbff550d79adee3769139d09bc11
Gh-Badr <ghazaoui.badr.tech@gmail.com>
2024-12-24 21:07:09 +0100

    Update configuration script

--- deployment/scripts/loadgenerator/configure.sh
+++ deployment/scripts/loadgenerator/configure.sh
@@ -3,28 +3,39 @@ set -e
 
 # Variables
 INVENTORY_FILE="deployment/ansible/inventory"
+SSH_KEY_PATH="$HOME/.ssh/loadgenerator_id_rsa"
+
+# Disable host key checking
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 # Change to the terraform directory
 cd deployment/terraform/loadgenerator
 
-# Retrieve the VM IP address
-VM_IP=$(terraform output -raw loadgenerator_vm_ip)
+# Retrieve the VM IP addresses
+MASTER_IPS=$(terraform output -json locust_master_ips | jq -r '.[]')
+WORKER_IPS=$(terraform output -json locust_worker_ips | jq -r '.[]')
 
 # Change to the root directory
 cd ../../..
 
 # Generate the inventory file
-cat <<EOF > ${INVENTORY_FILE}
-[loadgenerator]
-${VM_IP} ansible_user=root ansible_ssh_private_key_file=~/.ssh/loadgenerator_id_rsa
-EOF
+echo "[locust_master]" > ${INVENTORY_FILE}
+for IP in ${MASTER_IPS}; do
+  echo "${IP} ansible_user=root ansible_ssh_private_key_file=${SSH_KEY_PATH}" >> ${INVENTORY_FILE}
+done
+echo "[locust_workers]" >> ${INVENTORY_FILE}
+for IP in ${WORKER_IPS}; do
+  echo "${IP} ansible_user=root ansible_ssh_private_key_file=${SSH_KEY_PATH}" >> ${INVENTORY_FILE}
+done
 
 echo "Inventory file generated at ${INVENTORY_FILE}"
 
 # Retrieve the external IP address of the frontend service
 FRONTEND_IP=$(kubectl get svc frontend-external -n default -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-# Run the Ansible playbook
-ansible-playbook -i ${INVENTORY_FILE} deployment/ansible/playbook.yml --extra-vars "frontend_ip=${FRONTEND_IP}"
+# Run the Ansible playbooks
+ansible-playbook -i ${INVENTORY_FILE} deployment/ansible/playbook_common.yml --extra-vars "frontend_ip=${FRONTEND_IP}"
+ansible-playbook -i ${INVENTORY_FILE} deployment/ansible/playbook_master.yml --extra-vars "frontend_ip=${FRONTEND_IP}"
+ansible-playbook -i ${INVENTORY_FILE} deployment/ansible/playbook_worker.yml --extra-vars "frontend_ip=${FRONTEND_IP}"
 
-echo "Load generator VM configured successfully."
+echo "Load generator VMs configured successfully."
